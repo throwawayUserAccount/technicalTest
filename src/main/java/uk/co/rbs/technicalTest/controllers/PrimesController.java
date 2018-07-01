e61549a65e1f9f78492ceb18d00d057607f8fb18
Rob Evans <rwevans1984@gmail.com>
2018-07-01 19:55:47 +0100

    reorg/renaming controller and package and factor out primes method to service layer

--- src/main/java/uk/co/rbs/technicalTest/controllers/PrimesController.java
+++ src/main/java/uk/co/rbs/technicalTest/controllers/PrimesController.java
@@ -1,16 +1,16 @@
-package uk.co.rbs.technicalTest.resources;
+package uk.co.rbs.technicalTest.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import uk.co.rbs.technicalTest.service.PrimesService;
 
 @RestController
 public class PrimesController {
 
     @GetMapping("/primes/{upto}")
-    public String getPrimes(@PathVariable("upto") int upto) {
-        return "{ \"upto\" : " +  upto + ", \"primes\" : [ 2, 3, 5, 7, 11 ]}";
+    public static String getPrimes(@PathVariable("upto") int upto) {
+        return PrimesService.primesUpto(upto);
     }
-
 
 }
--- src/test/unit/uk/co/rbs/technicalTest/PrimesResourceTest.java
+++ src/test/unit/uk/co/rbs/technicalTest/PrimesResourceTest.java
@@ -10,8 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import uk.co.rbs.technicalTest.resources.PrimesController;
-
+import uk.co.rbs.technicalTest.controllers.PrimesController;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PrimesController.class)
